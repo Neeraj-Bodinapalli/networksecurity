968f98c170e86a0e2ec28b876b3b777517b1a051
Neeraj-Bodinapalli <cs23b1024@iiitdm.ac.in>
2025-12-15 12:35:58 +0530

    15-12

---
+++ network_security/components/data_ingestion.py
@@ -0,0 +1,10 @@
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+
+# configuration of Data ingestion 
+
+from network_security.entity.config_entity import DataIngestionConfig
+
+import os
+import sys
+import numpy
