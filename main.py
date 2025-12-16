32250895dc3e737afc1f9941b957d293ebbde67d
Neeraj-Bodinapalli <cs23b1024@iiitdm.ac.in>
2025-12-16 10:16:56 +0530

    model trainer done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from network_security.components.data_ingestion import DataIngestion
 from network_security.components.data_validation import DataValidation
 from network_security.components.data_transformation import DataTransformation
+from network_security.components.model_trainer import ModelTrainer
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig 
+from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -28,6 +29,13 @@ if __name__=="__main__":
         datatransformationartifact=datatransformation.initiate_data_transformation()
         logging.info(" data transformation done")
         print(datatransformationartifact)
+
+        logging.info("model trainer started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_trans_artifact=datatransformationartifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("model trainer done")
+        print(model_trainer_artifact)
 
 
     except Exception as e:
